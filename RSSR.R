bfad824fbb55c8f250de3a186cf4d4f7d3f19394
opheelorraine <66388539+opheelorraine@users.noreply.github.com>
2020-08-06 14:20:11 -0600

    deleting unnesscary code

--- RSSR.R
+++ RSSR.R
@@ -7,18 +7,14 @@ library(gsubfn)
 cd247<-read.fasta("sequence.fasta")
 #store DNA sequence for cd247 into cd247seq
 cd247seq<-cd247[[1]]
-cd247seq[1:50]
+
 #change cd247seq from vector of characters to string
 c2s(cd247seq)
-#create table of all the 7 character sequences in cd247 gene
-count(cd247seq,7)
-cd247table<-count(cd247seq,7)
-#give number of results for "cactcta"
-cd247table[["cactcta"]]
-cd247table[["tactgtg"]]
 
 library(Biostrings)
 vighmotif<-DNAStringSet(c("cacagct","cacaatg","cacagtg"))
 
 matchPDict(vighmotif, cd247seq)
+matchPattern(vighmotif,cd247seq)
+vmatchPattern(vighmotif,cd247seq)
 
